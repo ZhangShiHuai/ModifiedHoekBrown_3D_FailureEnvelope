39a75ff6fd1e8cfa3636f786d8b1b2d503033f08
Shihuai Zhang <shihuaizhang.xh1@gmail.com>
2021-09-24 22:06:31 +0200

    Create IntermediateStress.m

---
+++ IntermediateStress.m
@@ -0,0 +1,26 @@
+%% Plot the intermediate principal stress effect
+sigma3_temp=[0;20;65;];
+[mm,~]=size(sigma3_temp);
+figure
+for i=1:mm
+    for sigma2_temp=sigma3_temp(i):0.5:924
+        [sigma1_temp] = solve_sigma1( 50,600,sigma2_temp,sigma3_temp(i) ,mi_best,ucs,1e-6);
+         if sigma2_temp>sigma1_temp
+            break
+         end
+        
+        plot(sigma2_temp,sigma1_temp,'.g','MarkerSize',1)
+        hold on
+    end
+    
+end
+hold on
+plot([0,500],[0,500])
+%
+plot(sigma2(1:21),sigma1(1:21),'mo')
+hold on
+plot(sigma2(22:41),sigma1(22:41),'go')
+hold on           
+           
+           
+           
